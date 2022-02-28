72e9cf114cf49d7bdaaf92ab7f49533058f847d6
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-02-28 15:11:12 +0600

    design navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,8 +14,23 @@ const Navbar = () => {
           <div className={styles.text}>01991166550</div>
         </div>
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt='' width="160" height="69" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt='' width="30" height="30" />
+          <div className={styles.counter}>4</div>
+        </div>
+      </div>
     </div>
   )
 }
